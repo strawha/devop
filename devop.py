1a3efa72d66c4f413396d19117c5a469376770b9
strawhat <31542477+strawha@users.noreply.github.com>
2021-03-09 03:23:37 +0530

    Update devop.py

--- devop.py
+++ devop.py
@@ -1,28 +1,41 @@
 import logging
+import math
 
-def sum(a,b):
-    return a+b
+def square_root(a):
+    return math.sqrt(a)
 
-def diff(a,b):
-    return a-b
+def factorial(a):
+    return math.factorial(a)
 
-def multiply(a,b):
-    return a*b
+def natural_log(a):
+    return math.log(a)
 
-def divide(a,b):
-    return a//b
+def power(a,b):
+    return math.pow(a,b)
 
 if __name__ == "__main__":
-    x = sum(4,2)
-    y = diff(4,2)
-    z = multiply(4,2)
-    print("sum: ",x)
-    print("diff: ",y)
-    print("multiply: ",z)
+    x = int(input("enter your choice 1) for square root 2) for factorial 3) for natural_log 4) for power: "))
+    if (x == 1):
+        y = int(input("enter the number for square root: "))
+        z = square_root(y)
+        print(z)
+    elif (x == 2):
+        y = int(input("enter the number for factorial: "))
+        z = factorial(y)
+        print(z)
+    elif (x == 3):
+        y = int(input("enter the number for natural log: "))
+        z = natural_log(y)
+        print(z)
+    else:
+        y = int(input("enter the first number: "))
+        y = int(input("enter the second number: "))
+        z = power(x,y)
+        print(z)
+
     logging.basicConfig(filename="logFile.txt",
                     filemode='a',
                     format='%(asctime)s %(levelname)s-%(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',level=logging.DEBUG)
     logging.info(x)
-    logging.info(y)
     logging.info(z)
