f3325acdac95a1398442f2675f5bde64f83455d1
strawhat <31542477+strawha@users.noreply.github.com>
2021-03-11 18:32:21 +0530

    Update devop.py

--- devop.py
+++ devop.py
@@ -17,25 +17,40 @@ if __name__ == "__main__":
     x = int(input("enter your choice 1) for square root 2) for factorial 3) for natural_log 4) for power: "))
     if (x == 1):
         y = int(input("enter the number for square root: "))
-        z = square_root(y)
-        print(z)
+        try:
+            z = square_root(y)
+            print(z)
+        except ValueError:
+            print("square root not possible for negative number")
+            z = 0
     elif (x == 2):
         y = int(input("enter the number for factorial: "))
-        z = factorial(y)
-        print(z)
+        try:
+            z = factorial(y)
+            print(z)
+        except ValueError:
+            print("factorial not defined for negative values")
+            z = 0
     elif (x == 3):
-        y = int(input("enter the number for natural log: "))
-        z = natural_log(y)
-        print(z)
-    else:
+        try:
+            y = int(input("enter the number for natural log: "))
+            z = natural_log(y)
+            print(z)
+        except ValueError:
+            print("natural log only possible for number greater than zero")
+            z = 0
+    elif (x == 4):
         a = int(input("enter the first number: "))
         b = int(input("enter the second number: "))
         z = power(a,b)
         print(z)
+    else:
+        print("you only have 4 choices to choose from")
+        z = 0
 
     logging.basicConfig(filename="logFile.txt",
                     filemode='a',
                     format='%(asctime)s %(levelname)s-%(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',level=logging.DEBUG)
     logging.info(x)
-    logging.info(z)
+    logging.info(z))
