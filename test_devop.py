25aafa064b2624f3612f86a2793b6f17b7d16ee4
strawhat <31542477+strawha@users.noreply.github.com>
2021-03-11 19:12:32 +0530

    Update test_devop.py

--- test_devop.py
+++ test_devop.py
@@ -4,16 +4,22 @@ def test_square_root():
     assert square_root(4) == 2.0
     assert square_root(70.5) == 8.396427811873332
     assert square_root(0) == 0.0
+    assert square_root(5) != 2.0
+    assert square_root(13) != 4.0
 
 def test_factorial():
     assert factorial(4) == 24
     assert factorial(0) == 1
     assert factorial(1) == 1
+    assert factorial(3) != 16
+    assert factorial(2) != 3
 
 def test_natural_log():
     assert natural_log(10) == 2.302585092994046
     assert natural_log(2.2) == 0.7884573603642703
     assert natural_log(1) == 0.0
+    assert natural_log(11) != 3.0
+    assert natural_log(12) != 5.0
 
 def test_power():
     assert power(2,3) == 8.0
@@ -21,3 +27,5 @@ def test_power():
     assert power(3,0) == 1.0
     assert power(0,4) == 0.0
     assert power(-2,3) == -8.0
+    assert power(0, 5) != 5.0
+    assert power(-2, 6) != -8.0
